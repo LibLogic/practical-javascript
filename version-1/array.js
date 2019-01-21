347fa2ec68b630b19e6674bfc39aad0013ddccbe
LibLogic <hdgknsn@gmail.com>
2019-01-21 17:03:32 +0000

    a way to delete a todo

--- version-1/array.js
+++ version-1/array.js
@@ -6,3 +6,9 @@ console.log('My Todos:', todos);
 
 // A way to sdd new todos
 todos.push('item 4');
+
+// A way to change a todo
+todos[0] = 'item 1 updated';
+
+// A way to delete a todo
+todos.splice(0, 1);
