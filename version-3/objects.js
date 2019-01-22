197d30f810cb441bdb122bc2f30d30fafa4fa02f
LibLogic <hdgknsn@gmail.com>
2019-01-22 02:52:52 +0000

    added method to change a todo

--- version-3/objects.js
+++ version-3/objects.js
@@ -3,5 +3,10 @@ var todoList = {
 	todos: ['item 1','item 2', 'item 4'],
 	displayTodos: function(){
 		console.log(this.todos);
+	},
+// method to change a todo		
+	changeTodo: function(position, newValue){
+		this.todos[position] = newValue;
+		this.displayTodos();		
 	}
 };
