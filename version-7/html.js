cc5304124d6417327c037e0ee08a39cd27fe7506
LibLogic <hdgknsn@gmail.com>
2019-01-26 01:26:38 +0000

    Revert "a little refactorization"
    This reverts commit cb6700136efdf53e729ad779b17f80ee876176cb.

--- version-7/html.js
+++ version-7/html.js
@@ -61,22 +61,13 @@ var todoList = {
 	}
 };
 
-// var displayTodosButton = document.getElementById('displayTodosButton');
-// var toggleAllButton = document.getElementById('toggleAllButton');
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
 
-// displayTodosButton.addEventListener('click', function(){
-//   todoList.displayTodos();
-// });
+displayTodosButton.addEventListener('click', function(){
+  todoList.displayTodos();
+});
 
-// toggleAllButton.addEventListener('click', function(){
-//   todoList.toggleAll();
-// })
-
-var handlers = {
-	displayTodos: function(){
-		todoList.displayTodos();
-	},
-	toggleAll: function(){
-		todoList.toggleAll();
-	}
-}
+toggleAllButton.addEventListener('click', function(){
+  todoList.toggleAll();
+})
